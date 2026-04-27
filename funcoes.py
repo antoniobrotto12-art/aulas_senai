e5077aa8dc8fe28cab41fd6de6be3e4903742f96
Antonio Gabriel <antonio.brotto12@gmail.com>
2026-04-27 16:21:29 -0300

    opa

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,14 @@
-print("olá GitHub ")
+
+
+def calcular_media(nota1, nota2, nota3):
+    soma = nota1 + nota2 + nota3
+    media = soma / 3
+    return media
+# Programa principal (fora da funçao)
+n1 = float(input("digite a primeira nota: "))
+n2 = float(input("digite a segunda nota: "))
+n3 = float(input("digite a terceira nota: "))
+
+resultado = calcular_media(n1, n2, n3)
+
+print(f"media final: ", resultado )
